585132b15ceec2ed0a5ae1aedf54ce62e9b7810f
Zubayer-Al-Mamun <zubayer.a.m83@gmail.com>
2023-10-05 11:03:54 +0600

    add bee: 1113 prob

---
+++ 1113_asending_and_descending.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ 
+int main() {
+    while(1){
+        int x,y;
+        scanf("%d %d", &x,&y);
+        if(x==y){
+            break;
+        }
+        else if(x>y){
+            printf("Decrescente\n");
+        }
+        else{
+            printf("Crescente\n");
+        }
+    }
+    return 0;
+}
